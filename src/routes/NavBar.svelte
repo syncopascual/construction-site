42f4b3a42120494b9536f3a539008bc0513db62c
Marcus Pascual <marcusjoaquim05@gmail.com>
2025-09-08 17:58:01 +0800

    feat: change spacing and text alignment of NavBar

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -19,7 +19,7 @@
     }}
 />
 <div
-    class="navbar flex h-24 items-stretch justify-center bg-royal-blue lg:space-x-60 {showNav
+    class="navbar flex h-24 items-stretch justify-between px-5 bg-royal-blue text-center lg:space-x-60 {showNav
         ? 'show'
         : 'hide'} z-1000 w-full {page.route.id === base ? 'absolute' : 'relative'}"
 >
